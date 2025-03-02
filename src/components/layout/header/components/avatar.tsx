35db5a0fa8430a16efa3a22de29eb2349bc96103
Владислав <vlados66625@mail.ru>
2025-03-02 19:46:44 +0300

    refactor: reducers перенесены в rootReducer fix: добавление доп проверки отрисовки фотографии пользователя

--- src/components/layout/header/components/avatar.tsx
+++ src/components/layout/header/components/avatar.tsx
@@ -4,10 +4,11 @@ import { authorizationSelectors } from '../../../../store/slices/authorization';
 
 export default function Avatar(): JSX.Element {
   const avatarUrl = useAppSelector(authorizationSelectors.avatarUrl);
+  const isAuth = useAppSelector(authorizationSelectors.isAuth);
 
   return (
     <div className="header__avatar-wrapper user__avatar-wrapper">
-      {avatarUrl !== '' && <img className="header__avatar user__avatar" src={avatarUrl} width={20} height={20} alt='photo user'></img>}
+      {avatarUrl !== '' && isAuth && <img className="header__avatar user__avatar" src={avatarUrl} width={20} height={20} alt='photo user'></img>}
     </div>
   );
 }
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,29 +3,23 @@ import { AppDispatch, State } from '../types/state';
 import { ActionCreatorsMapObject, AsyncThunk, bindActionCreators } from '@reduxjs/toolkit';
 import { useMemo } from 'react';
 
-// Хук для получения dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-// Типизированный хук для получения состояния
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
 
-// Типизация для экшенов
 type BoundActions<Actions extends ActionCreatorsMapObject> = {
   [Key in keyof Actions]: Actions[Key] extends AsyncThunk<infer ReturnType, infer ThunkArg, object>
   ? BoundAsyncThunk<ReturnType, ThunkArg>
   : Actions[Key];
 };
 
-// Типизация для асинхронных экшенов
 type BoundAsyncThunk<ReturnType, ThunkArg> = (
   arg?: ThunkArg
 ) => Promise<ReturnType>;
 
-// Хук для привязки экшенов
 export function useActionCreators<Actions extends ActionCreatorsMapObject>(
   actions: Actions
 ): BoundActions<Actions> {
   const dispatch = useAppDispatch();
-  // Используем as для обхода ограничений типизации bindActionCreators
   return useMemo(() => bindActionCreators(actions, dispatch) as BoundActions<Actions>, [actions, dispatch]);
 }

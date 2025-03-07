e96f04e8a942ca98f44fb4b36937710caaace775
Владислав <vlados66625@mail.ru>
2025-03-07 23:23:06 +0300

    fix: Gallery отрисовывает максимум 6 картинок

--- src/pages/offer/components/gallery.tsx
+++ src/pages/offer/components/gallery.tsx
@@ -6,7 +6,7 @@ export default function Gallery({ images }: GalleryProps): JSX.Element {
   return (
     <div className="offer__gallery-container container" data-testid="gallery">
       <div className="offer__gallery">
-        {images.map((image) => (
+        {images.slice(0,6).map((image) => (
           <div className="offer__image-wrapper" key={image}>
             <img className="offer__image" src={image} alt="Photo studio" />
           </div>

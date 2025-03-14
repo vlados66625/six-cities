3b29713c6209a36f8bb1ff73848e91991148e8c5
Владислав <vlados66625@mail.ru>
2025-03-14 12:22:51 +0300

    release: подготовка проекта в выгрузке на netlify

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -14,13 +14,13 @@ type MapProps = {
 }
 
 const defaultCustomIcon = leaflet.icon({
-  iconUrl: '/img/pin.svg',
+  iconUrl: 'img/pin.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39],
 });
 
 const currentCustomIcon = leaflet.icon({
-  iconUrl: '/img/pin-active.svg',
+  iconUrl: 'img/pin-active.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39],
 });
